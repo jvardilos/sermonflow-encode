e631cbe77222f4346baf51f674683c33ff4c97bd
jvardi <jvardi@gideon.local>
2026-06-25 13:08:18 -0700

    format the python files

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # Take from a native google cloud storage and put into a in/ directory
 from decode import decode
 from encode import encode
@@ -7,7 +6,7 @@ from encode import encode
 def main():
     encode("test.probundle", "in", "Presentation")
     decode("test.probundle", "output", True)
-    
+
 
 if __name__ == "__main__":
     main()
